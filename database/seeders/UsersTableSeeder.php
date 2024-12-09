6a756803550a0718ee02321ccb0bc969ca40cb72
ChunDeik <chundeik24@gmail.com>
2024-12-09 11:59:18 +0800

    feat: UserTableSeeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class UserTableSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     public function run()
     {
